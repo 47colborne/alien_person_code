5d08ccfc046519b896288019bb8ffd61582c1e20
“vdutz” <vlad@retailcommon.com>
2018-12-14 08:57:23 -0500

    Working version of 3-second delay for UI, but all in one file

--- src/display.js
+++ src/display.js
@@ -1,24 +1,49 @@
-const display = (character1, character2) => {
+// const game = require('./game')
+const game = require('./game')
 
-process.stdout.write(`${character1.output_value()}          ${character2.output_value()}`);
+const continuouslyUpdateStateAndDisplay = () => {
+	console.log("HERE:")
+	console.log(game)
+	const characterState = game.getCharacterState()
+	const character1State = character1State[0]
+	const character2State = character1State[1]
 
-const readline = require('readline');
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
+	// display()
 
-process.stdin.on('keypress', (str, key) => {
-  if (key.ctrl && key.name === 'c') {
-    process.exit();
-  } else {
-    character1.performCommand(str);
-    character2.performCommand(str);
+	setInterval(display(character1State, character2State), 1000);
 
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
+	    
 
-    process.stdout.write(`${character1.output_value()}          ${character2.output_value()}`);
-  }
-});
+	// process.stdout.write(`${character1.output_value()}          ${character2.output_value()}`);
+
+	// const readline = require('readline');
+	// readline.emitKeypressEvents(process.stdin);
+	// process.stdin.setRawMode(true);
+
+	// process.stdin.on('keypress', (str, key) => {
+	//   if (key.ctrl && key.name === 'c') {
+	//     process.exit();
+	//   } else {
+	//     character1.performCommand(str);
+	//     character2.performCommand(str);
+
+	//     process.stdout.clearLine();
+	//     process.stdout.cursorTo(0);
+
+	//     process.stdout.write(`${character1.output_value()}          ${character2.output_value()}`);
+	//   }
+	// });
 }
 
-module.exports = display;
+continuouslyUpdateStateAndDisplay()
+
+const display = (character1, character2) => {
+	process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+
+    process.stdout.write(`${character1State.output_value()}          ${character2State.output_value()}`);
+}
+
+module.exports = {
+	continuouslyUpdateStateAndDisplay: continuouslyUpdateStateAndDisplay
+}
